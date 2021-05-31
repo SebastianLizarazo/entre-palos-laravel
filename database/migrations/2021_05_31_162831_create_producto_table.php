8820eb069ded30148565a7e201b08c6b296ab8af
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-05-31 14:45:10 -0500

    Creacion de la migracion correspondiente a la tabla producto sin llaves foraneas

--- database/migrations/2021_05_31_162831_create_producto_table.php
+++ database/migrations/2021_05_31_162831_create_producto_table.php
@@ -14,7 +14,19 @@ class CreateProductoTable extends Migration
     public function up()
     {
         Schema::create('producto', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id')->unique();
+            $table->string('Nombre');
+            $table->smallInteger('Tamano')->unsigned();
+            $table->enum('ReferenciaTamano',['ml','Lt','Kg','gr','Oz','Lb']);
+            $table->string('Referencia');
+            $table->integer('PrecioBase')->unsigned();
+            $table->integer('PrecioUnidadTrabajador')->unsigned();
+            $table->integer('PrecioUnidadVenta')->unsigned();
+            $table->enum('PresentacionProdcuto',[
+                'Lata','Botella vidrio','Botella plastico','Tetrapack','Predeterminado',
+                 'Icopor','Vaso vidio','Vaso plastico','Tasa']);
+            $table->smallInteger('CantidadProducto')->unsigned();
+            $table->enum('Estado',['Activo','Inactivo']);
             $table->timestamps();
         });
     }
