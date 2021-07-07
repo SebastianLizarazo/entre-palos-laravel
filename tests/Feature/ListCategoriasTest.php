26c3b67efbeac0fc5286609365f22ce8455b0c8a
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-07-07 08:47:50 -0500

    Construccion de tests 2

--- tests/Feature/ListCategoriasTest.php
+++ tests/Feature/ListCategoriasTest.php
@@ -2,21 +2,67 @@
 
 namespace Tests\Feature;
 
+use App\Models\Categoria;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ListCategoriasTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_can_see_all_categorias()
     {
-        $response = $this->get('/');
+        //$this->withoutExceptionHandling();
+
+        $categoria = Categoria::create([
+            'nombre' => 'Categoria 69',
+            'tipo' => 'Comida',
+            'estado' => 'Inactivo'
+        ]);
+
+        $categoria2= Categoria::create([
+            'nombre' => 'Categoria 32',
+            'tipo' => 'Comida',
+            'estado' => 'Activo'
+        ]);
+
+        $response = $this->get(route('categorias.index'));
+
 
         $response->assertStatus(200);
+
+        $response->assertViewIs('modules.categoria.index');
+
+        $response->assertViewHas('categorias');
+
+        $response->assertSee($categoria->nombre);
+        $response->assertSee($categoria2->nombre);
+    }
+
+    public function test_can_see_individual_categoria()
+    {
+        $categoria = Categoria::create([
+            'nombre' => 'Categoria 69',
+            'tipo' => 'Comida',
+            'estado' => 'Inactivo'
+        ]);
+
+        $categoria2= Categoria::create([
+            'nombre' => 'Categoria 32',
+            'tipo' => 'Comida',
+            'estado' => 'Activo'
+        ]);
+
+        $response = $this->get(route('categorias.show', $categoria));
+
+        $response->assertSee($categoria->nombre);
+
+        $response->assertDontSee($categoria2->nombre);
     }
 }
--- tests/Feature/ListProductosTest.php
+++ tests/Feature/ListProductosTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Categoria;
+use App\Models\Producto;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -19,16 +21,92 @@ class ListProductosTest extends TestCase
     {
         //$this->withoutExceptionHandling();//Para deshabilitar la captura de esepciones de los test para que nos muestre todoo el error
 
-        //Debemos crear un proyecto para verificar que si lo podemos ver
+        $categoria = Categoria::create([//Primero creo una categoria para que no haya probrema con la asignacion de la llave foranea en el producto
+            'nombre' => 'Categoria 69',
+            'tipo' => 'Comida',
+            'estado' => 'Inactivo'
+        ]);
+
+        $producto = Producto::create([
+            'nombre' => 'Canre asada',
+            'tamano' => 200,
+            'referencia_tamano' => 'ml',
+            'referencia' => 021,
+            'precio_base' => 2200,
+            'precio_unidad_trabajador' => 4000,
+            'precio_unidad_venta' => 5000,
+            'presentacion_producto' => 'Tetrapack',
+            'cantidad_producto' => 5,
+            'categoria_id' => $categoria->id,//asigno el id de la categoria que cree anteriormente
+            'estado' => 'Activo'
+        ]); //Debemos crear un proyecto para verificar que si lo podemos ver
+
+        $producto2 = Producto::create([
+            'imagen_producto' => 'sjdhaksdhalsdhksdasd',
+            'nombre' => 'Agua molida',
+            'tamano' => 300,
+            'referencia_tamano' => 'ml',
+            'referencia' => 032,
+            'precio_base' => 2200,
+            'precio_unidad_trabajador' => 4000,
+            'precio_unidad_venta' => 5000,
+            'presentacion_producto' => 'Tetrapack',
+            'cantidad_producto' => 5,
+            'categoria_id' => $categoria->id,//asigno el id de la categoria que cree anteriormente
+            'estado' => 'Activo'
+        ]);
 
         $response = $this->get(route('productos.index'));
 
-        //Verificamos que la vista que nos retorna sea la que le pedimos
+        $response->assertViewIs('modules.producto.index'); //Verificamos que la vista que nos retorna es la que le pedimos
 
-        //Verificamos si una variable esta disponible en la vista
+        $response->assertViewHas('productos'); //Verificamos si una variable esta disponible en la vista
 
         $response->assertStatus(200);
 
-        //Verificamos que se pueda ver el nombre del proyecto en la respuesta
+        $response->assertSee($producto->nombre); //Verificamos que se pueda ver el nombre del proyecto en la respuesta
+        $response->assertSee($producto2->nombre);
+    }
+
+    public function test_can_see_individual_producto()
+    {
+        $categoria = Categoria::create([//Primero creo una categoria para que no haya probrema con la asignacion de la llave foranea en el producto
+            'nombre' => 'Categoria 69',
+            'tipo' => 'Comida',
+            'estado' => 'Inactivo'
+        ]);
+
+        $producto = Producto::create([
+            'nombre' => 'Canre asada',
+            'tamano' => 200,
+            'referencia_tamano' => 'ml',
+            'referencia' => 021,
+            'precio_base' => 2200,
+            'precio_unidad_trabajador' => 4000,
+            'precio_unidad_venta' => 5000,
+            'presentacion_producto' => 'Tetrapack',
+            'cantidad_producto' => 5,
+            'categoria_id' => $categoria->id,//asigno el id de la categoria que cree anteriormente
+            'estado' => 'Activo'
+        ]);
+        $producto2 = Producto::create([
+            'imagen_producto' => 'sjdhaksdhalsdhksdasd',
+            'nombre' => 'Agua molida',
+            'tamano' => 300,
+            'referencia_tamano' => 'ml',
+            'referencia' => 032,
+            'precio_base' => 2200,
+            'precio_unidad_trabajador' => 4000,
+            'precio_unidad_venta' => 5000,
+            'presentacion_producto' => 'Tetrapack',
+            'cantidad_producto' => 5,
+            'categoria_id' => $categoria->id,//asigno el id de la categoria que cree anteriormente
+            'estado' => 'Activo'
+        ]);
+
+        $response = $this->get( route('productos.show',$producto));
+
+        $response->assertSee($producto->referencia_tamano);//Verificamos si podemos ver el show de este producto en especifico
+        $response->assertDontSee($producto2->nombre);//Verificamos si efectivamente NO podemos ver el show del producto 2
     }
 }
